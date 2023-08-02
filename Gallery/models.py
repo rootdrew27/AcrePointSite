5d9f5ab9043aa2a855e38a791abc65e0aa71be8d
Andrew Root <root.drew27@gmail.com>
2023-08-01 21:29:07 -0500

    moved compressImage to new feature branch

--- Gallery/models.py
+++ Gallery/models.py
@@ -5,14 +5,6 @@ from io import BytesIO
 from django.core.files import File
 
 
-def compress_image(image:models.FileField):
-    img = Image.open(image)
-    if img.mode != "RGB":
-        img = img.convert("RGB")
-    img_ouput = BytesIO()
-    img.save(img_ouput, 'JPEG', quality=80)
-    compressed_image = File(img_ouput, name=image.name)
-    return compressed_image
 
 # Create your models here.
 class Category(models.Model):
@@ -33,10 +25,6 @@ class ImageCard(models.Model):
     #ToDo: order by 'most clicks' or by Diegos preference
     class Meta:
         ordering = ["-date_created"]
-        
-    def save(self, *args, **kwargs):
-        self.image = compress_image(self.image)
-        super().save(*args, **kwargs)
 
     def resize_image(self):
         img = Image.open(self.image, 'r')

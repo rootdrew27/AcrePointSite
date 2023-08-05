9a4a57cd56bd7fbf2c23e556df7bfb5eeed65916
Andrew Root <root.drew27@gmail.com>
2023-08-04 22:32:35 -0500

    created unworking image display script

--- Gallery/static/Gallery/scripts/main.js
+++ Gallery/static/Gallery/scripts/main.js
@@ -1,9 +1,13 @@
-let categoryButtons,
-    allCategoriesButton;
+
 
 $(function() {
     console.log("Document is Ready");
 
+    let dg_categoryButtons,
+    dg_allCategoriesButton,
+    dg_categoiresToDisplay,
+    dg_imgsToDisplay, //(when this array is empty, all images are shown)
+    dg_imgs;
 
     //Event listeners
 
@@ -12,35 +16,77 @@ $(function() {
     //If button has class all-categories -> remove 'activated' from all other buttons
     //If button does NOT have class all-categories -> remove 'activated' from 'All' button
 
+    dg_imgs = $('.dg-img');
+
     //list of buttons
-    categoryButtons = $('.btn.category');
+    dg_categoryButtons = $('.btn.dg-category');
     //ALL button
-    allCategoriesButton = $('.btn.category.allcategories');
+    dg_allCategoriesButton = $('.btn.dg-allcategories');
 
-
-    categoryButtons.on('click', function () {
+    //ToDo: break into multiple events 
+    //One for the 'All' button, and events for each other button (these would be the same event) 
+    dg_categoryButtons.on('click', function () {
 
         var thisButton = $(this);
 
-        if (thisButton.attr('activated') === undefined) {
+        //if thisButton is not activated...
+        if (thisButton.attr('dg-activated') === undefined) {
 
-            thisButton.attr('activated', true);
+            thisButton.attr('dg-activated', true);// activate it 
 
-            if(thisButton.hasClass('allcategories') != false) {
-                categoryButtons.each( function(i, e) {
+            
+            if(thisButton.hasClass('dg-allcategories') === true) { //if this is the 'All' button..
+                dg_categoryButtons.each(function(i, e) { // deactivate other buttons
                     if(i === 0){ return; } //ie. continue
-                    $(e).removeAttr('activated');
+                    $(e).removeAttr('dg-activated');
                 })
+                //show all images
+                dg_imgs.each(function(i, e){
+                    $(this).removeAttr('hidden');
+                });
+                dg_categoiresToDisplay = []; //reset
+                dg_imgsToDisplay = []; //reset (when this array is empty, all images are shown)
             }
-            else {
-                allCategoriesButton.removeAttr('activated');
+            else { //thisButton is not the 'All' button
+                dg_allCategoriesButton.removeAttr('dg-activated'); //deactivate 'All' button
+
+                var thisCateogry = $(this).attr('dg-category');
+
+                dg_categoiresToDisplay.append(thisCateogry); //add category to display list
+
+                dg_imgs.each(function(index, thisImage){
+                                        
+                    var img_categories = $(thisImage).attr('dg-categories').split(','); //current image's categories
+
+                    img_categories.forEach(function(i, img_category){
+                        if(img_category === thisCateogry){
+                            dg_imgsToDisplay.append(thisImage);
+                        }
+                    })
+                })
+                if (dg_categoiresToDisplay.length === 1){
+                    dg_imgs.attr('hidden','true');
+                    $(dg_imgsToDisplay).removeAttr('hidden');
+                }
+                else { // dg_categoriesToDisplay.length > 1
+                    $(dg_imgsToDisplay).removeAttr('hidden');
+                }
+                // dg_imgsToDisplay = []; //reset 
             }
         }
-        else {
-            thisButton.removeAttr('activated');
+        else { //this button is activated
+            thisButton.removeAttr('dg-activated'); //deactivate it
         }
 
     });
+
+    //Add images to array
+    //Keep track of 'activated' categories
+    //Display images which match any category in the category array
+
+
+
+
     
 });
 
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Mock Site for Diego
 
+# Color Scheme
+
 Colors:
 --dark-purple: #0d0221ff;
 --dark-slate-gray: #2c514cff;
@@ -9,16 +11,25 @@ Colors:
 --ghost-white: #fafaffff;
 --seal-brown: #6a381fff;
 
+# Django Apps
+
 Apps:
 Gallery -- photo gallery of jobs
 Landing Page -- description of business
 Consultation -- Forms for setting up Consultation
 
-# ToDo:
+# ToDo
 - implement 'url' template tag
 - Do image sorting on the frontend 
+- Apply Bootstrap5.3 to the GalleryApp
 
 # Post-MVP
 - Ensure that files exist b4 sending to client
 - optimize querys (in Gallery IndexView, a better should be made to get all the data needed)
 - Alter (in some way) the Gallery models s.t. the model fields accurately reflect the models' relationships
+- improve JS efficency: run tests to see if it is better to query the dom, or to loop over small number of elements
+
+# Notes
+
+- loading in scripts after all other scripts (of the base.html file) may cause conflicts 
+  you may want to load in jquery on a per app basis (by default) but, you may be able to use logic to prevent scripts from loading twice
